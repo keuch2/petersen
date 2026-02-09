636ac26df09c5407c1952297b38d275fb0ba2e56
Boris  Dedoff <keuch2@gmail.com>
2026-02-09 16:42:13 -0300

    Actualizar header.php y agregar nuevos logos de marcas

--- includes/header.php
+++ includes/header.php
@@ -158,3 +158,12 @@
     </div>
 </div>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MXFGHT83T8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MXFGHT83T8');
+</script>
